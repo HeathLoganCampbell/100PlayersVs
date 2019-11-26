cd0de83c1b0bcd59f879e505579293576092dc0f
Heath Logan Campbell <github@heathlogancampbell.com>
2019-11-26 17:35:53 +1300

    init

---
+++ src/main/java/games/bevs/hundredvs/HundredVSPlugin.java
@@ -0,0 +1,12 @@
+package games.bevs.hundredvs;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class HundredVSPlugin extends JavaPlugin
+{
+    @Override
+    public void onEnable()
+    {
+        System.out.println("Yeet enabled");
+    }
+}
